09cf30ce081e9bb6266b0850dddcabd9ff0c5aef
Uti Favour <utifavour15@gmail.com>
2024-10-25 14:17:59 +0100

    #Mobile view redesign

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -10,8 +10,8 @@ const Hero = () => {
     <div className='lg:flex lg:mx-20 mx-5 mt-10'>
         <div className='mt-20'>
           <div className='space-y-10'>
-          <h1 className='text-blue-700 lg:text-7xl text-4xl font-medium lg:max-w-[80%]'>Discover Your Next Great Adventure <i className="fa-solid fa-plane-departure"></i></h1>
-            <p className='text-gray-500 lg:max-w-[70%]'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugiat rem natus quos, accusantium repellendus facere pariatur aperiam ullam id quasi vitae vel odit dicta consequatur porro molestias a magnam rerum!</p>
+          <h1 className='text-blue-700 text-center lg:text-left lg:text-7xl text-6xl font-black lg:max-w-[80%]'>Discover Your Next  Adventure</h1>
+            <p className='text-gray-500 text-center lg:text-left lg:max-w-[70%]'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugiat rem natus quos, accusantium repellendus facere pariatur aperiam ullam id quasi vitae vel odit dicta consequatur porro molestias a magnam rerum!</p>
             <div className='text-black space-x-4 text-center lg:flex-row flex-col flex'>
                 <Link href={'google.com'} className='py-3 px-10 bg-blue-700 text-white rounded-3xl'>Book Consultation <i className="fa-solid fa-arrow-right-long ml-1"></i>
 
@@ -19,6 +19,7 @@ const Hero = () => {
                 <Link href={'google.com'} className='py-3 px-10 bg-[#F1F2F4] text-blue-500 rounded-3xl lg:block hidden'>View Features</Link>
             </div>
           </div>
+            <div className='lg:block hidden'>
             <div className='flex space-x-6 text-blue-700 lg:mt-28 mt-20'>
                 <div>
                     <h2 className='font-medium text-4xl'>50+</h2>
@@ -29,10 +30,11 @@ const Hero = () => {
                     <p className='text-blue-500 max-w-48'>Lorem ipsum dolor sit amet consectetur uno.</p>
                 </div>
             </div>
+            </div>
         </div>
 
         <div className='lg:mt-0 mt-10'>
-            <Image src={plane} width={950} height={500} alt='plane image' className='rounded-3xl'/>
+            <Image src={plane} width={950} height={500} alt='plane image' className='rounded-3xl max-h-[95vh]'/>
         </div> 
     </div>
     </>
