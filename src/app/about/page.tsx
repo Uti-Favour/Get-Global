23b8ee4e41ac0987e4a4d4d7c14ffbbc77fd3440
Uti Favour <utifavour15@gmail.com>
2024-11-05 19:54:45 +0100

    #About section

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,16 +2,22 @@ import React from 'react'
 import Navbar from '@/components/Navbar'
 import Footer from '@/components/Footer'
 import Image from 'next/image'
-import about_img from '../../../public/mission.jpg'
+import about_img from '../../../public/plane_image.jpg'
 
 const About = () => {
   return (
     <div>
     <Navbar/>
-    <div className='flex mx-20'>
-      <h1 className='text-black'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore architecto suscipit cum est doloremque dolore tenetur dolorem, eaque pariatur molestiae fugit harum ratione iure, error autem nemo. Quam porro, deserunt nobis expedita velit veritatis asperiores dignissimos eaque facilis enim fugit aliquid earum doloribus unde error natus distinctio! Beatae, obcaecati facilis.</h1>
+    <div className='flex justify-between mx-20 mt-20 items-center'>
+    <div className='space-y-10'>
+      <h1 className='text-[#054378] lg:text-left lg:text-7xl text-4xl font-black'>About Get Global</h1>
+    <p className='text-gray-500 max-w-3xl leading-relaxed'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore architecto suscipit cum est doloremque dolore tenetur dolorem, eaque pariatur molestiae fugit harum ratione iure, error autem nemo. Quam porro, deserunt nobis expedita velit veritatis asperiores dignissimos eaque facilis enim fugit aliquid earum doloribus unde error natus distinctio! Beatae, obcaecati facilis.</p>
+    <p className='text-gray-500  max-w-3xl leading-relaxed'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore architecto suscipit cum est doloremque dolore tenetur dolorem, eaque pariatur molestiae fugit harum ratione iure, error autem nemo. Quam porro, deserunt nobis expedita velit veritatis asperiores dignissimos eaque facilis enim fugit aliquid earum doloribus unde error natus distinctio! Beatae, obcaecati facilis.</p>
+    <p className='text-gray-500  max-w-3xl leading-relaxed'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore architecto suscipit cum est doloremque dolore tenetur dolorem, eaque pariatur molestiae fugit harum ratione iure, error autem nemo. Quam porro, deserunt nobis expedita velit veritatis asperiores dignissimos eaque facilis enim fugit aliquid earum doloribus unde error natus distinctio! Beatae, obcaecati facilis.</p>
+    <p className='text-gray-500  max-w-3xl leading-relaxed'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore architecto suscipit cum est doloremque dolore tenetur dolorem, eaque pariatur molestiae fugit harum ratione iure, error autem nemo. Quam porro, deserunt nobis expedita velit veritatis asperiores dignissimos eaque facilis enim fugit aliquid earum doloribus unde error natus distinctio! Beatae, obcaecati facilis.</p>
+    </div>
       <div>
-        <Image src={about_img} width={50} height={50} alt='About Image'/>
+        <Image src={about_img} width={500} height={500} alt='About Image' className='rounded-3xl'/>
       </div>
     </div>
     <Footer/>
