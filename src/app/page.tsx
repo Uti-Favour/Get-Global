f008141d416b0008cb74fcc7ad228abc6d3b86a8
Uti Favour <utifavour15@gmail.com>
2024-11-11 02:57:09 +0100

    Fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import '@fortawesome/fontawesome-free/css/all.css';
 import Navbar from '@/components/Navbar';
 import Hero from '@/components/Hero'
-import Services from '@/components/Services';
 import Footer from '@/components/Footer';
 import Offers from '@/components/Offers';
 import Features from '@/components/Features';
