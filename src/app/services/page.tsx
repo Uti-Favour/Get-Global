c23c8a5ad81c5c91adfc762950fa321eed102305
Uti Favour <utifavour15@gmail.com>
2024-11-13 01:13:22 +0100

    Changes

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -4,44 +4,64 @@ import Footer from '@/components/Footer'
 import Image from 'next/image'
 import service_image from '../../../public/service_1.jpg'
 import service_image_2 from '../../../public/service_2.jpg'
+import service_image_3 from '../../../public/plane_image.jpg'
+
 
 
 const services = () => {
   return (
     <>
       <Navbar />
-      <div className='lg:flex justify-between lg:mx-36 mx-5 items-center'>
+      <div className='lg:flex justify-between lg:mx-36 mx-5 items-center lg:mt-10 mt-5'>
       <div className='lg:block hidden'>
-          <Image src={service_image_2} width={550} height={200} alt='Image' className='rounded-3xl overflow-hidden' />
+          <Image src={service_image_2} width={550} height={200} alt='Image' className='max-h-[700px] object-cover' />
         </div>
-        <div className=' text-black mt-16 space-y-6'>
+        <div className=' text-black mt-16 space-y-8'>
           
           {/* <span className='text-[#054378] font-bold'>Future of Travel</span> */}
-          <h1 className='lg:text-5xl text-xl text-[#054378] font-semibold max-w-xl'>Title for service section</h1>
+          <h1 className='lg:text-5xl text-4xl text-[#054378] font-semibold max-w-xl'>Title for service section</h1>
           <p className='max-w-xl text-gray-500'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus animi aliquid asperiores, magni nobis modi consequatur ex veniam corporis qui mollitia neque fugiat commodi ullam, dignissimos alias molestias ab atque.</p>
-          <div className='space-x-4'>
-          <button className='py-3 px-5 rounded-3xl bg-[#054378] text-white'>See our offers</button>
+          <div className='lg:space-x-4'>
+          <button className='py-3 px-8 rounded-3xl bg-[#054378] text-white'>See our offers</button>
           <button className='py-3 px-5 underline rounded-3xl border-[#054378] text-gray-600'>Learn More</button>
         </div>
         <div className='lg:hidden block lg:mt-0 mt-10'>
-          <Image src={service_image_2} width={500} height={200} alt='Image' className='rounded-3xl overflow-hidden' />
+          <Image src={service_image_2} width={550} height={200} alt='Image' />
         </div>
         </div>
        
       </div>
       <div className='lg:flex justify-between lg:mx-36 mx-5 items-center'>
-        <div className=' text-black mt-16 space-y-6'>
+        <div className=' text-black mt-16 space-y-8'>
           {/* <span className='text-[#054378] font-bold'>Future of Travel</span> */}
-          <h1 className='text-5xl text-[#054378] font-semibold max-w-xl'>Title for service section</h1>
+          <h1 className='lg:text-5xl text-4xl text-[#054378] font-semibold max-w-xl'>Title for service section</h1>
           <p className='max-w-xl text-gray-500'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus animi aliquid asperiores, magni nobis modi consequatur ex veniam corporis qui mollitia neque fugiat commodi ullam, dignissimos alias molestias ab atque.</p>
           <div className='space-x-4'>
-          <button className='py-3 px-5 rounded-3xl bg-[#054378] text-white'>See our offers</button>
-          <button className='py-3 px-5 rounded-3xl border border-[#054378] text-gray-600'>Learn More</button>
+          <button className='py-3 lg:px-10 px-28 rounded-3xl bg-[#054378] text-white'>See our offers</button>
         </div>
         </div>
         <div className='lg:mt-0 mt-10'>
-          <Image src={service_image} width={500} height={200} alt='Image' className='rounded-3xl' />
+          <Image src={service_image} width={550} height={200} alt='Image' className='max-h-[700px]' />
         </div>
+      </div>
+      <div className='lg:flex justify-between lg:mx-36 mx-5 items-center lg:mt-10 mt-5'>
+      <div className='lg:block hidden'>
+          <Image src={service_image_3} width={550} height={200} alt='Image' className='max-h-[700px] object-cover' />
+        </div>
+        <div className=' text-black mt-16 space-y-8'>
+          
+          {/* <span className='text-[#054378] font-bold'>Future of Travel</span> */}
+          <h1 className='lg:text-5xl text-4xl text-[#054378] font-semibold max-w-xl'>Title for service section</h1>
+          <p className='max-w-xl text-gray-500'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus animi aliquid asperiores, magni nobis modi consequatur ex veniam corporis qui mollitia neque fugiat commodi ullam, dignissimos alias molestias ab atque.</p>
+          <div className='lg:space-x-4'>
+          <button className='py-3 px-8 rounded-3xl bg-[#054378] text-white'>See our offers</button>
+          <button className='py-3 px-5 underline rounded-3xl border-[#054378] text-gray-600'>Learn More</button>
+        </div>
+        <div className='lg:hidden block lg:mt-0 mt-10'>
+          <Image src={service_image_3} width={550} height={200} alt='Image' />
+        </div>
+        </div>
+       
       </div>
       <Footer />
     </>
